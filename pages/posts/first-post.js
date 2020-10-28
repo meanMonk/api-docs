4c8077e7a0c56b8c8c932224dbfb9c9100eb50ee
sahil kashetwar <kashetwartechlab@gmail.com>
2020-10-28 19:53:44 +0530

    first module

---
+++ pages/posts/first-post.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import Layout from '../../components/Layout';
+export default function FirstPage() {
+	return (
+		<Layout>
+			<h1>Post Goes here!</h1>
+			<p>Description goes here for the pages of documents</p>
+		</Layout>
+	);
+}
